4135f7e0f966d4f4fbc59cbcd570f1fe516846b6
ramneek008 <singhramneek274@gmail.com>
2020-12-11 01:56:29 +0530

    admin login servlet

--- src/dao/DBConnectivity.java
+++ src/dao/DBConnectivity.java
@@ -9,15 +9,14 @@ public class DBConnectivity {
 	static {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM", "D@ta00Found");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM", "D@ta00Found");  //Enter your password
 			System.out.println("Connection done");
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
 	
-	public static Connection getConnection()
-	{
+	public static Connection getConnection() {		
 		return con;
 	}
 }
---
+++ src/servlets/AdminLogin.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+
+public class AdminLogin extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	{
+		String user = req.getParameter("user");
+		String pwd = req.getParameter("pwd");
+		
+		if(user.equals("admin1") && pwd.equals("123456"))
+		{
+			RequestDispatcher rd = req.getRequestDispatcher("admin.html");
+			rd.include(req,res);
+		}
+		else
+		{
+			PrintWriter pw = res.getWriter();
+			pw.println("<h1>Username or password incorrect</h1>");
+			RequestDispatcher rd = req.getRequestDispatcher("adminlogin.html");
+			rd.include(req, res);
+			
+		}
+	}
+
+}
